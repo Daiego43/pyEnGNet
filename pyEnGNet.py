ffa98820f626f35ae74c58276ee6fdef63d49bf3
Daiego43 <diedelcha@gmail.com>
2022-04-18 17:43:03 +0200

    Se ha terminado la primera parte del algoritmo consistente del major voting.
    Con los resultados obtenidos (una lista de aristas con las relaciones mas prometedoras) se debe generar un grafo al cual se le tiene que aplicar una estrategia voraz

--- pyEnGNet.py
+++ pyEnGNet.py
@@ -2,7 +2,10 @@ import pandas as pd
 import scipy.stats as scp
 import numpy as np
 from tqdm import tqdm
-from numba import jit
+from sklearn.metrics.cluster import normalized_mutual_info_score
+import warnings
+
+warnings.filterwarnings('ignore')
 
 
 def extract_nparray(filename):
@@ -12,8 +15,14 @@ def extract_nparray(filename):
     return cleandataset
 
 
+sameval = 0.5
+
+
 class PyEnGNet:
-    def __init__(self, nparr=None):
+
+
+
+    def __init__(self, nparr=None, nmi_th=sameval, spearman_th=sameval, kendall_th=sameval):
         if nparr is not None:
             self.maindata = nparr
         else:
@@ -21,6 +30,10 @@ class PyEnGNet:
 
         self.row_size = len(self.maindata)
         self.column_size = len(self.maindata[0])
+
+        self.spearman_threshold = spearman_th
+        self.kendall_threshold = kendall_th
+        self.nmi_threshold = nmi_th
 
     # Calcular el coeficiente de correlación de spearman (Slow but correct way)
     def spearman_test(self):
@@ -42,13 +55,88 @@ class PyEnGNet:
         return kendallcorrs, kendallpvalues
 
     # Calcular el NMI
+    def nmi_test(self, precission):
+        nmi_values = np.zeros((self.row_size, self.row_size))
+        for i in range(len(self.maindata)):
+            for j in range(len(self.maindata)):
+                rnd = lambda x: round(x, precission)
+                v1 = list(map(rnd, self.maindata[i]))
+                v2 = list(map(rnd, self.maindata[j]))
+                nmi_values[i][j] = normalized_mutual_info_score(v1, v2)
+        return nmi_values
+
+    def single_nmi(self, arr1, arr2, precission=1):
+        ans = 0
+        rnd = lambda x: round(x, precission)
+        v1 = np.array(list(map(rnd, arr1)))
+        v2 = np.array(list(map(rnd, arr2)))
+        corr = normalized_mutual_info_score(v1, v2)
+        if corr >= self.nmi_threshold:
+            ans = 1
+        return ans, corr
+
+    def single_spearman(self, arr1, arr2):
+        ans = 0
+        corr, pv = scp.spearmanr(arr1, arr2)
+        if corr >= self.spearman_threshold:
+            ans = 1
+        return ans, abs(corr)
+
+    def single_kendall(self, arr1, arr2):
+        ans = 0
+        corr, pv = scp.kendalltau(arr1, arr2)
+        if corr >= self.kendall_threshold:
+            ans = 1
+        return ans, abs(corr)
+
+
+    def engnet_1_0(self):
+        engnet_accepted_values = []
+        major_voting = 0
+        for i in range(self.row_size):
+            for j in range(i+1, self.row_size):
+                # Las dos filas que vamos a utilizar
+                v = self.maindata[i]
+                w = self.maindata[j]
+                tests = []
+
+                # Aplicamos nmi y kendall y sumamos al major voting
+                nmi = self.single_nmi(v, w)
+                kend = self.single_kendall(v, w)
+
+                major_voting += nmi[0] + kend[0]
+
+                # Agregamos las respuestas de ambos tests a una lista que servirá para el calculo de los pesos
+                tests.append(nmi)
+                tests.append(kend)
+
+                # Si los resultados anteriores no son concluyentes se procede a una tercera prueba
+                if major_voting >= 1:
+                    spear = self.single_spearman(v, w)
+                    major_voting += spear[0]
+                    tests.append(spear)
+
+                # Si se obtiene una mayoría, entonces la relacion se considera apta, se guarda la arista que los
+                # representa y se calcula el peso asociado a dicha arista
+                if major_voting >= 2:
+                    engnet_accepted_values.append([i, j, self.calculate_weight(tests)])
+
+                major_voting = 0
+
+        return engnet_accepted_values
+
+    def calculate_weight(self, tests):
+        weight = []
+        for test in tests:
+            if bool(test[0]):
+                weight.append(test[1])
+        return np.mean(weight)
 
     def __str__(self):
         return f"PyEnGNet Object with shape ({self.row_size},{self.column_size})"
-    # Calcular el coeficiente de correlación de spearman (Slow but correct way)
 
 
 if __name__ == "__main__":
-    peg = PyEnGNet(extract_nparray("datasample_big.csv"))
-    peg.spearman_test()
-    peg.kendall_test()
+    peg = PyEnGNet(extract_nparray("datasample_shittys.csv"))
+    peg.engnet_1_0()
+    pd.DataFrame()
