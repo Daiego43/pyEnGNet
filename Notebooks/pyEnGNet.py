69b7b2db05df381875d0a70124d80c2df4a19550
Daiego43 <diedelcha@gmail.com>
2022-05-12 23:07:16 +0200

    Código completo de enmgnet1.0

--- Notebooks/pyEnGNet.py
+++ Notebooks/pyEnGNet.py
@@ -25,7 +25,7 @@ def intervals(total, partes):
 
 
 class PyEnGNet:
-    def __init__(self, nparr=None, nmi_th=sameval, spearman_th=sameval, kendall_th=sameval, pearson_th=sameval, cores=int(mp.cpu_count()/2)):
+    def __init__(self, nparr=None, nmi_th=sameval, spearman_th=sameval, kendall_th=sameval, pearson_th=sameval, readded_th=0.7, cores=int(mp.cpu_count()/2)):
         if nparr is not None:
             self.maindata = nparr
         else:
@@ -39,6 +39,7 @@ class PyEnGNet:
         self.nmi_threshold = nmi_th
         self.pearson_threshold = pearson_th
 
+        self.readded_edges_threshold = readded_th
         self.ncores = cores
 
     # Calcular el coeficiente de correlación de spearman (Slow but correct way)
@@ -196,12 +197,43 @@ class PyEnGNet:
         return edges
 
     def engnet_1_0(self):
-        aristas = self.mainmethod()
+        oedges = self.mainmethod()
         G = nx.Graph()
-        G.add_edges_from(aristas)
-        G = nx.maximum_spanning_tree(G, weight='weight', algorithm="kruskal")
-        edges = nx.to_edgelist(G)
-        return G, edges
+        G.add_edges_from(oedges)
+        G2 = nx.maximum_spanning_tree(G, weight='weight', algorithm="kruskal")
+        G3 = self.readd_edges(G, G2)
+        fedges = nx.to_edgelist(G3)
+        return G3, fedges
+
+
+    def readd_edges(self, graph0, graph2):
+
+        origin_edges = nx.to_edgelist(graph0)
+        tree_edges = nx.to_edgelist(graph2)
+
+        # Obtain eliminated edges
+        eliminated_edges = [edge for edge in origin_edges if edge not in tree_edges]
+
+        #Obtain hubs
+        mean_degree = round(np.mean([graph2.degree(node) for node in graph2.nodes if graph2.degree(node) > 2]))
+        hubs = []
+        for node in graph2.nodes:
+            if graph2.degree(node) >= mean_degree:
+                hubs.append(node)
+
+        readded = []
+        for node in hubs:
+            for e in eliminated_edges:
+                if (node in e) and (e not in readded) and (e[2]['weight'] > self.readded_edges_threshold):
+                    readded.append(e)
+
+        print(readded)
+        G3 = nx.Graph()
+        G3.add_edges_from(nx.to_edgelist(graph2))
+        G3.add_edges_from(readded)
+
+        return G3
+
 
     def __str__(self):
         return f"PyEnGNet Object with shape ({self.row_size},{self.column_size})"
@@ -213,3 +245,5 @@ if __name__ == "__main__":
     data = df.to_numpy()
     peg = PyEnGNet(nparr=data)
     G, aristas = peg.engnet_1_0()
+
+
